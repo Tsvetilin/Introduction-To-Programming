aacd49eb7b8c59adb57dec168e952be09d9e8dd1
georgi5634 <56654793+georgi5634@users.noreply.github.com>
2024-10-07 16:59:29 +0300

    Update 1_Week_Solutions.cpp

--- Practicum/1_Week_Solutions.cpp
+++ Practicum/1_Week_Solutions.cpp
@@ -1,1 +1,77 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+int main()
+{
+    /*
+        zad 1
+    int a = 198;
+    std::cout << !(a % 2) <<std::endl;*/
+    
+    
+    /*
+    2 zad
 
+    int a, b;
+    std::cin >> a ;
+    std::cin >> b;
+    std::cout << a / b << std::endl;
+    std::cout << a % b << std::endl;
+    
+    */
+
+    /*
+    
+    zad 3
+    double a;
+    std::cin >> a;
+    
+    std::cout << (9.0 / 5) * a + 32 << std::endl;*/
+
+    /* 
+        zad 4
+    double a, b;
+    std::cin >> a;
+    std::cin >> b;
+    std::cout << 2*a + 2*b << std::endl;
+    std::cout << a*b << std::endl;*/
+
+
+     /* 
+      5 zad
+     
+     int seconds;
+    std::cin >> seconds;
+    int days = seconds / (3600 * 24);
+    int hours = (seconds - days * (3600 * 24))/3600;
+    int minutes = ((seconds -  days * (3600 * 24)) - hours*3600)/60;
+     seconds = ((seconds - days * (3600 * 24)) - hours * 3600)% 60;
+    
+    std::cout << "days:" << days  << "   " << "hours  "  << hours << "minutes:  "<< minutes <<"seconds  "  << seconds ;
+    */
+
+    /* 
+        6 zad
+    int a;
+    std::cin >> a;
+    std::cout << a / 10; */
+
+    /*
+     7 zad
+    
+    int a;
+    std::cin >> a;
+    std::cout << ((a >= 2) && (a <= 6)) || 0;*/
+
+    
+    
+   
+
+
+
+    
+
+
+
+}
