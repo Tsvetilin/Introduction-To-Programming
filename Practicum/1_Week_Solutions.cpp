2d928e3e05b407f3c9a4a006ff04054e0efd65b0
GeorgiParnarev <131038567+GeorgiParnarev@users.noreply.github.com>
2024-10-07 17:03:56 +0300

    Update 1_Week_Solutions.cpp

--- Practicum/1_Week_Solutions.cpp
+++ Practicum/1_Week_Solutions.cpp
@@ -1,1 +1,129 @@
+#include <iostream>
+
+int main()
+{
+	// zad 1
+
+	/*int a;
+
+	std::cin >> a;
+
+	std::cout << !(a % 2);*/
+
+	// zad 2
+
+	/*int a;
+	std::cin >> a;
+
+	int b;
+	std::cin >> b;
+
+	std::cout << a / b << '\n' << a % b;*/
+
+	// zad 3
+
+	/*int a;
+
+	std::cin >> a;
+
+	std::cout << (9.0 / 5.0) * a + 32;*/
+
+	// zad 4
+
+	/*double a;
+	std::cin >> a;
+
+	double b;
+	std::cin >> b;
+
+	std::cout << 2 * (a + b) << '\n' << a * b;*/
+
+	// zad 5
+
+	/*int SECONDS_IN_DAY = 86400;
+	int SECONDS_IN_HOUR = 3600;
+	int SECONDS_IN_MINUTE = 60;
+
+	int a, b;
+	std::cin >> a >> b;
+
+	int time = a * 1000 + b;
+
+	int days = time / SECONDS_IN_DAY;
+	time -= days * SECONDS_IN_DAY;
+
+	int hours = time / SECONDS_IN_HOUR;
+	time -= hours * SECONDS_IN_HOUR;
+
+	int minutes = time / SECONDS_IN_MINUTE;
+	time -= minutes * SECONDS_IN_MINUTE;
+
+	std::cout << days << " days, " << hours << " hours, " << minutes << " minutes and " << time << " seconds";*/
+
+	// zad 6
+
+	/*int a;
+
+	std::cin >> a;
+
+	a /= 10;
+
+	std::cout << a;*/
+
+	// zad 7
+
+	/*int a;
+
+	std::cin >> a;
+
+	std::cout << (a >= 2 && a <= 6);*/
+
+	// zad 8
+
+	// zad 9
+
+	/*int n;
+
+	std::cin >> n;
+
+	std::cout << (n % 2) * (n + 1) / 2 + (n + 1) * (n / 2);*/
+
+	// zad 10
+
+	/*int a, b, c, d;
+
+	std::cin >> a >> b >> c >> d;
+
+	std::cout << (a * a == b) && (a * b == c) && (a * c == d);*/
+
+	// zad 11
+
+	/*double a;
+	std::cin >> a;
+
+	double b;
+	std::cin >> b;
+
+	b += a;
+	a = b - a;
+	b -= a;*/
+
+	// zad 12
+
+	// zad 13
+
+	// zad 14
+
+	/*int a, b, c, d, e;
+	std::cin >> a >> b >> c >> d >> e;
+
+	std::cout << (a % 2) * a + (b % 2) * b + (c % 2) * c + (d % 2) * d + (e % 2) * e;*/
+
+	// zad 15
+
+	/*int a, b, c;
+	std::cin >> a >> b >> c;
+
+	std::cout << (a + b > c) && (a + c > b) && (b + c > a);*/
+}
 
