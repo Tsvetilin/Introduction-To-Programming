0a2c6600b4577df1e6f0bfec1618f75e200a3710
Boris Petrov <130251821+BorisPetrov05@users.noreply.github.com>
2024-10-07 22:04:10 +0300

    Update 1st Week.cpp
    Борис Петров 1MI0600528

--- Practicum/1st Week.cpp
+++ Practicum/1st Week.cpp
@@ -10,7 +10,7 @@ int main()
 	/*int num1, num2;
 	std::cin >> num1 >> num2;
 	std::cout << num1 / num2 << std::endl << num1 % num2;*/
-	
+
 	//zad3
 	/*double celsius;
 	std::cin >> celsius;
@@ -22,52 +22,69 @@ int main()
 	std::cin >> w >> h;
 	std::cout << w * 2 + h * 2 << std::endl << w * h;*/
 
-    //zad5
+	//zad5
 	/*int sec;
 	std::cin >> sec;
 	int days = sec / 86400;
 	int hours = sec / 3600 - days*24;
 	int mins = (sec % 3600) / 60;
 	int secs = sec - (60 * (mins = sec / 60));
-	std::cout << days << "days, " << hours << " hours," 
+	std::cout << days << "days, " << hours << " hours,"
 		<< mins << " mins, and " << secs << " seconds.";*/
 
-	//zad6
-	/*int num;
-	std::cin >> num;
-	std::cout << num / 10;*/
+		//zad6
+		/*int num;
+		std::cin >> num;
+		std::cout << num / 10;*/
 
-	//zad7
-	/*int grade;
-	std::cin >> grade;
-	std::cout << (grade > 2 && grade < 6);*/
+		//zad7
+		/*int grade;
+		std::cin >> grade;
+		std::cout << (grade > 2 && grade < 6);*/
 
-	//zad8
-	/*int a, b, c, d;
-	std::cin >> a >> b;
-	std::cin >> c >> d;
-	std::cout << (a <= c && b >= c && c <= d) || (c <= a && a <= d && d <= b);*/
+		//zad8
+		/*int a, b, c, d;
+		std::cin >> a >> b;
+		std::cin >> c >> d;
+		std::cout << (a <= c && b >= c && c <= d) || (c <= a && a <= d && d <= b);*/
 
-	//zad9
+		//zad9
+		/*int n;
+		std::cin >> n;
+		std::cout << n*(n + 1) / 2;*/
+
+		//zad10
+		/*int a, b, c, d;
+		std::cin >> a >> b >> c >> d;
+		std::cout << (a * b == c) && (b * c == d) && (a * a == b);*/
+
+		//zad11
+		/*double a, b;
+		std::cin >> a >> b;
+		a = a + b;
+		b = a - b;
+		a = a - b;
+		std::cout << a << " " << b;*/
+
+		//zad12
+		/*double a, b;
+		std::cin >> a >> b;
+		std::cout << (a > b) * a + (b >= a) * b;
+		*/
+
+		//zad13
 	/*int n;
 	std::cin >> n;
-	std::cout << n*(n + 1) / 2;*/
+	std::cout << !(n > -100 && n < 100) * n % 10 + (n > -100 && n < 100) * n;*/
+	
+	//zad14
+	/*int a, b, c, d, e;
+	std::cin >> a >> b >> c >> d >> e;
+	std::cout << a * (a % 2 != 0) + b * (b % 2 != 0) + c * (c % 2 != 0) + d * (d % 2 != 0) + e * (e % 2 != 0);*/
 
-	//zad10
-	/*int a, b, c, d;
-	std::cin >> a >> b >> c >> d;
-	std::cout << (a * b == c) && (b * c == d) && (a * a == b);*/
-
-	//zad11
-	/*double a, b;
-	std::cin >> a >> b;
-	a = a + b;
-	b = a - b;
-	a = a - b;
-	std::cout << a << " " << b;*/
-
-	//zad12
-	/*double a, b;
-	std::cin >> a >> b;
-	std::cout << (a > b) * a + (b >= a) + b;*/
+	//zad15
+	/*int a, b, c;
+	std::cin >> a >> b >> c;
+	std::cout << (a * a + b * b == c * c);*/
+	return 0;
 }
