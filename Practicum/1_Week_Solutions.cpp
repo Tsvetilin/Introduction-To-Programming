ba0d00a164a8f52c8c68bca4f5d32f34d7fdb143
alexsrebrov <alexsrebrov@gmail.com>
2024-10-07 17:00:51 +0300

    Alexandar 1_Week_Solutions.cpp

--- Practicum/1_Week_Solutions.cpp
+++ Practicum/1_Week_Solutions.cpp
@@ -1,1 +1,145 @@
+#include <iostream>
+// purva
+
+//int main()
+//{
+//    int a;
+//    std::cin >> a;
+//    bool isTrue = a%2==0;
+//    std::cout << isTrue;
+//}
+
+
+
+//vtora
+
+//int main()
+//{
+//    int a, b;
+//    std::cin >> a >> b;
+//    std::cout << a / b << std::endl;
+//    std::cout << a % b;
+//}
+
+//treta
+
+//int main()
+//{
+//    double degreesCelcius;
+//    std::cin >> degreesCelcius;
+//    double degreesFarenhait = (degreesCelcius * (9.0 / 5.0)) + 32;
+//    std::cout << degreesFarenhait;
+//}
+
+//4
+//int main()
+//{
+//	double length, width;
+//	std::cin >> width >> length;
+//	double perimeter = 2 * (width + length);
+//	double area = length * width;
+//	std::cout << perimeter << std::endl;
+//	std::cout << area;
+//}
+
+
+//5
+//int main()
+//{
+//	int seconds,minutes,hours,days;
+//	std::cin >> seconds;
+//	 days = seconds / (60 * 60 * 24);
+//	seconds -= days * 60 * 60 * 24;
+//	 hours = seconds / (60 * 60);
+//	seconds -= hours * 60 * 60;
+//	 minutes = seconds/ 60;
+//	seconds -= minutes * 60;
+//	std::cout << days << " days, " << hours << " hours, " << minutes << " minutes, " << seconds << " seconds";
+//}
+
+
+
+//6
+//int main()
+//{
+//	int a;
+//	std::cin >> a;
+//	std::cout << a / 10;
+//}
+
+//7
+//int main()
+//{
+//	int a;
+//	std::cin >> a;
+//	bool isTrue = a >= 2 && a <= 6;
+//	std::cout << isTrue;
+//}
+
+
+
+//8
+//int main()
+//{
+//	int a, b, c, d;  // [a,b]  [c,d]
+//	std::cin >> a >> b >> c >> d;
+//	bool isTrue = a <= d && b >= c;
+//	std::cout << isTrue;
+//}
+
+
+//9
+//int main()
+//{
+//	int n;
+//	std::cin >> n;
+//	int sum = 1 + n;
+//	double groups = n / 2.0;
+//    double print = sum * groups;
+//	std::cout <<print;	
+//}
+
+
+
+//10
+//int main()
+//{
+//	int a,b,c,d;
+//	std::cin >> a>>b>>c>>d;
+//	double q = b / a;
+//	bool isTrue = c/d == q && d/c==q;
+//	std::cout << isTrue;
+//}
+
+
+//11
+//int main()
+//{
+//	double  a, b;
+//	std::cin >> a >> b;
+//	 a = a + b;
+//	 b = a - b;
+//	 a = a - b;
+//	std::cout << a<< " and " << b;
+//}
+
+
+//12
+//int main()
+//{
+//	double  a, b;
+//	std::cin >> a >> b;
+//	double bigger = (a >= b ? a : b);
+//	std::cout << bigger;
+//}
+
+//13
+//int main()
+//{
+//	int  a;
+//	std::cin >> a;
+//	a = (a / 100 == 0 ? a : a % 10);
+//	std::cout << a;
+//}
+
 
