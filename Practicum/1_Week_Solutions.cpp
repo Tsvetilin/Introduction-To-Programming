a6600721f8435ff77622d57ff7f0731bb45a08e1
tyhristova <teodora.yu.hristova@gmail.com>
2024-10-07 17:00:14 +0300

    Update 1_Week_Solutions.cpp

--- Practicum/1_Week_Solutions.cpp
+++ Practicum/1_Week_Solutions.cpp
@@ -1,1 +1,68 @@
+// FirstProject.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+	// 1
+  /*  std::cout << "hello world!\n";
+	int num=0;
+	std:: cin >> num;
+	std::cout << num % 2;*/
+
+	// 2
+   /* int num;
+	int num2;
+	std::cin >> num;
+	std::cin >> num2;
+	int nominator = num / num2;
+	int remainder = num % num2;
+
+	std::cout << nominator << std::endl;
+	std::cout << remainder;*/
+
+	// 3
+	/*int celsius;
+	std::cin >> celsius;
+	double far = (9.0 / 5) * celsius + 32;
+	std::cout << far;*/
+
+	//4
+	/*double width;
+	double lenght;
+	std::cin >> width >> lenght;
+	std::cout << 2 * width + 2 * lenght << std::endl;
+	std::cout << width * lenght;*/
+
+	//5
+		/*int num;
+		std::cin >> num;
+		int days = num / 86400;
+		std::cout << days << std:: endl;
+		int r1 = num % 86400;
+		int hours = r1 / 3600;
+		std::cout << hours << std:: endl;
+		int r2 = r1 % 3600;
+		int minutes = r2 / 60;
+		std::cout << minutes << std::endl;
+		int r3 = r2 % 60;
+		int seconds = r3;
+		std::cout << seconds;*/
+
+
+
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
 
