24b35bba2eba0564517a28fc712057c29100b3f2
Stanislav Trendafilov <114089296+Stanislav-Trendafilov@users.noreply.github.com>
2024-10-07 17:00:08 +0300

    Update 1_Week_Solutions.cpp

--- Practicum/1_Week_Solutions.cpp
+++ Practicum/1_Week_Solutions.cpp
@@ -1,1 +1,81 @@
 
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	//1
+	/*int number;
+	cin >> number;
+	cout << !(number % 2)<<endl;
+	cout << (number % 2 == 0 ? 1 :0);*/
+
+	//2
+	/*int num1;
+	int num2;
+	cin >> num1;
+	cin >> num2;
+	cout << num1 / num2 << endl;
+	cout << num1 % num2 << endl;*/
+
+	//3
+	/*int cels;
+	cin >> cels;
+	cout << (9.0 / 5) * cels + 32;*/
+
+	//4
+	//double length;
+	//double width;
+	//cin >> length;
+	//cin >> width;
+	//cout << 2*width+2*length<<endl;
+	//cout << width * length;
+
+	//5
+	/*int number;
+	cin >> number;
+	int days = number / 86400.0;
+	number -= (days * 86400);
+
+	int hours = number / 3600.0;
+	number -= (hours * 3600);
+
+	int minutes = number / 60.0;
+	number -= (minutes * 60);
+
+	int seconds = number;
+
+	cout << days << " days" << endl;
+	cout << hours << " hours" << endl;
+	cout << minutes << " minutes" << endl;
+	cout << seconds << " seconds" << endl;*/
+
+	//6
+	/*int number;
+	cin >> number;
+	int num1 = number % 10;
+
+	cout << num1;*/
+
+	//7
+	/*int number;
+	cin >> number;
+	cout << (number == 1 || number == 2 || number == 3 || number == 4 || number == 5 || number == 6);*/
+
+	//8
+	/*int a, b, c, d;
+	cin >> a >> b >> c >> d;
+	cout << (a >= c || a <= d)||(b>=c||b<=d);*/
+
+	//9
+	/*int num;
+	cin >> num;
+	cout << num * (num + 1) / 2;*/
+
+	//10
+
+}
+
+
