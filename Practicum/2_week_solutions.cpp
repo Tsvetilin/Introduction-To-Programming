0b280883257e31a0025233214754c0222c42ff97
Felipe Urra <87881375+felipeUrra@users.noreply.github.com>
2024-10-14 19:56:07 +0300

    Update 2_week_solutions.cpp
    Solution of the 7 exercise finished

--- Practicum/2_week_solutions.cpp
+++ Practicum/2_week_solutions.cpp
@@ -127,33 +127,30 @@ int main()
 
     //7
     
-    int n1, n2, n3;
-    std::cout << "Introduce 3 numbers: ";
-    std::cin >> n1 >> n2 >> n3;
+    // int n1, n2, n3;
+    // std::cout << "Introduce 3 numbers: ";
+    // std::cin >> n1 >> n2 >> n3;
 
-    int aux;
+    // int aux;
+    // int i = 0;
 
+    // while (i<2) {
+    //     if (n1 > n2) {
+    //     aux = n1;
+    //     n1 = n2; 
+    //     n2 = aux;
+    //     }
+
+    //     if (n2 > n3) {
+    //         aux = n2;
+    //         n2 = n3;
+    //         n3 = aux;
+    //     }
+
+    //     i++;
+    // }
     
-    if (n1 > n2) {
-        aux = n1;
-        n1 = n2;
-        n2 = aux;
-    }
-
-    if (n2 > n3) {
-        aux = n2;
-        n2 = n3;
-        n3 = aux;
-    }
-
-    if (n1 > n3) {
-        aux = n1;
-        n1 = n3;
-        n3 = aux;
-    }
-
-    std::cout << n3 << " " << n2 << " " << n1;
-
+    // std::cout << n1 << " " << n2 << " " << n3;
 
 }
 
