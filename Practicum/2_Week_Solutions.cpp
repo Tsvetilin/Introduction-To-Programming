0a55c5be8260db0f22024ab348eeaadc286e800e
Polina Staneva <polina.staneva@yahoo.com>
2024-10-14 16:59:03 +0300

    Polina_Staneva_2_Week_Solutions.cpp
    Polina Staneva My Solutions

--- Practicum/2_Week_Solutions.cpp
+++ Practicum/2_Week_Solutions.cpp
@@ -1,1 +1,110 @@
+    //zad1
+    /*
+    int n;
+    std::cin >> n;
+    if (n > 0) {
+        std::cout << "Positive";
+    }
+    else if (n == 0) {
+        std::cout << "Zero";
+    }
+    else {
+        std::cout << "Negative";
+    }*/
 
+    //zad2
+
+    /*int n = 0;
+    std::cin >> n;
+    switch (n)
+    {
+    case 1:
+        std::cout << "I";
+        break;
+    case 2:
+        std::cout << "II";
+        break;
+    case 3:
+        std::cout << "III";
+        break;
+    case 4:
+        std::cout << "IV";
+        break;
+    case 5:
+        std::cout << "V";
+        break;
+    case 6:
+        std::cout << "VI";
+        break;
+    case 7:
+        std::cout << "VII";
+        break;
+    case 8:
+        std::cout << "VIII";
+        break;
+    case 9:
+        std::cout << "IX";
+        break;
+
+    default:
+        std::cout << "Number is out of range";
+        break;
+    }
+}*/
+
+//zad3
+
+    /*int n = 0;
+    std::cin >> n;
+    if (n % 400 == 0) {
+        std::cout << "Leap Year";
+    }
+    else if ((n % 4 == 0) && (n % 100 != 0)) {
+
+        std::cout << "Leap Year";
+    }
+    else {
+        std::cout << "Not a Leap Year";
+    }*/
+
+  //zad4
+
+    /*int n = 0;
+    std::cin >> n;
+    switch (n)
+    {
+    case 1:
+        std::cout << "Monday";
+        break;
+    case 2:
+        std::cout << "Tuesday";
+        break;
+    case 3:
+        std::cout << "Wednesday";
+        break;
+    case 4:
+        std::cout << "Thursday";
+        break;
+    case 5:
+        std::cout << "Friday";
+        break;
+    case 6:
+        std::cout << "Saturday";
+        break;
+    case 7:
+        std::cout << "Sunday";
+        break;
+
+    default:
+        std::cout << "Not Valid value";
+        break;
+    }*/
+
+    //zad5
+
+    /*int first = 0, second = 0;
+    std::cin >> first;
+    std:: cin >> second;
+    std::cout << (first > second ? first : second);*/
+   
+    //zad6
