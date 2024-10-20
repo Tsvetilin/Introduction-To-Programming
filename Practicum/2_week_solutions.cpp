77d8c867efb661f66c21a9bf607b08fdfbddab88
Felipe Urra <87881375+felipeUrra@users.noreply.github.com>
2024-10-20 19:12:02 +0300

    Update 2_week_solutions.cpp
    all the solutions added

--- Practicum/2_week_solutions.cpp
+++ Practicum/2_week_solutions.cpp
@@ -118,12 +118,42 @@ int main()
 
     //6
     
-    // Isn't finished
-    
-    /*char letter = ' ';
-    std::cout << "Introduce a letter: ";
-    std::cin >> letter;*/
+    // char letter = ' ';
+    // std::cout << "Introduce a letter: ";
+    // std::cin >> letter;
 
+    // if ((letter >= 'a' && letter <= 'z') || (letter >= 'A' && letter <= 'Z')) {
+    //     if (letter >= 'a' && letter <= 'z') {
+    //         std::cout << (char)(letter - 32) << '\n';
+    //     } else {
+    //         std::cout << (char)(letter + 32) << '\n';
+    //     }
+        
+
+    //     switch (letter) {
+    //         case 'a':
+    //         case 'e':
+    //         case 'i':
+    //         case 'o':
+    //         case 'u':
+    //         case 'A':
+    //         case 'E':
+    //         case 'I':
+    //         case 'O':
+    //         case 'U':
+    //             std::cout << "Vowel";
+    //             break;
+
+    //         default:
+    //             std::cout << "Consonant";
+    //             break;
+    //     }
+
+
+    // } else {
+    //     std::cout << "Invalid character";
+    // }
+    
 
 
     //7
@@ -169,5 +199,143 @@ int main()
     //     std::cout << "On the circle";
     // }
     
+    //9
+
+    // float bound1, bound2, n;
+    // const float EPSILON = 0.0001;
+
+    // std::cout << "Introduce 3 numbers: ";
+    // std::cin >> bound1 >> bound2 >> n;
+
+    // if (n > bound1 && n < bound2) {
+    //     std::cout << "The number is in the interval. The interval is open.";
+    // } else if (fabs(bound1 - n) < EPSILON || fabs(bound2 - n) < EPSILON) {
+    //     std::cout << "The number is in the interval. The interval is closed.";
+    // } else {
+    //     std::cout << "The number isn't in the interval.";
+    // }
+    
+    //10
+
+    // int originalNum;
+    // std::cin >> originalNum;
+
+    // int dig1 = originalNum / 1000;
+    // int dig2 = (originalNum % 1000) / 100;
+    // int dig3 = (originalNum % 100) / 10;
+    // int dig4 = originalNum % 10;
+
+    // int num1 = dig1 * 10 + dig4;
+    // int num2 = dig2 * 10 + dig3;
+
+    // if (num1 < num2) {
+    //     std::cout << num1 << " < " << num2;
+    // } else if (num1 > num2) {
+    //     std::cout << num1 << " > " << num2;
+    // } else {
+    //     std::cout << num1 << " == " << num2;
+    // }
+
+    //11
+
+    // int day, month, year;
+    // std::cin >> day >> month >> year;
+
+    // bool leapYear = year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+    // bool validDate = year >= 0 ? 1:0;
+
+    // switch (month) {
+    //     case 1:
+    //         validDate = validDate && (day >= 1 && day <= 31);
+    //         break;
+    //     case 3:
+    //         validDate = validDate && (day >= 1 && day <= 31);
+    //         break;
+    //     case 5:
+    //         validDate = validDate && (day >= 1 && day <= 31);
+    //         break;
+    //     case 7:
+    //         validDate = validDate && (day >= 1 && day <= 31);
+    //         break;
+    //     case 8:
+    //         validDate = validDate && (day >= 1 && day <= 31);
+    //         break;
+    //     case 10:
+    //         validDate = validDate && (day >= 1 && day <= 31);
+    //         break;
+    //     case 12:
+    //         validDate = validDate && (day >= 1 && day <= 31);
+    //         break;
+        
+    //     case 2:
+    //         validDate = validDate && day >= 1 && day <= (leapYear ? 29 : 28);
+    //         break;
+        
+    //     case 4:
+    //         validDate = validDate && (day >= 1 && day <= 30);
+    //         break;
+    //     case 6:
+    //         validDate = validDate && (day >= 1 && day <= 30);
+    //         break;
+    //     case 9:
+    //         validDate = validDate && (day >= 1 && day <= 30);
+    //         break;
+    //     case 11:
+    //         validDate = validDate && (day >= 1 && day <= 30);
+    //         break;
+    
+    // default:
+    //     validDate = false;
+    //     break;
+    // }
+
+    // std::cout << validDate;
+    
+    //12
+
+    // int x1, y1, x2, y2, pointX, pointY;
+    // std:: cin >> x1 >> y1 >> x2 >> y2 >> pointX >> pointY;
+
+    // if ((pointX > x1 && pointX < x2) && (pointY > y1 && pointY < y2)) {
+    //     std::cout << "In the rectangle.";
+    // } else if ((pointX == x1 || pointX == x2) || (pointY == y1 || pointY == y2)) {
+    //     std::cout << "On the rectangle.";
+    // } else {
+    //     std::cout << "Out of the rectangle.";
+    // }
+    
+    //13
+
+    // int a, b, c;
+    // std::cin >> a >> b >> c;
+
+    // int d = b * b - 4 * a * c;
+
+    // if (d > 0) {
+    //     std::cout << 2;
+    // } else if (d == 0 ) {
+    //     std::cout << 1;
+    // } else {
+    //     std::cout << 0;
+    // }
+
+    //14
+
+    // char a1, a2, b1, b2, c;
+    // std::cin >> a1 >> a2 >> b1 >> b2 >> c;
+
+    // a1 += 32;
+    // a2 += 32;
+    
+    // bool isInTheUnion = (c >= a1 && c <= a2) ||  (c >= b1 && c <= b2);
+    // bool isInTheCrossSection = (c >= a1 && c <= a2) && (c >= b1 && c <= b2);
+    // bool isInTheDifference = (c >= a1 && c <= a2) && !(c >= b1 || c <= b2);
+    // bool isOnlyInOneSet = isInTheUnion && !isInTheCrossSection;
+
+    // std::cout << (isInTheUnion ? "true" : "false") << " " 
+    //     << (isInTheCrossSection ? "true" : "false") << " " 
+    //     << (isInTheDifference ? "true" : "false") << " " 
+    //     << (isOnlyInOneSet ? "true" : "false");
+
 
 }
