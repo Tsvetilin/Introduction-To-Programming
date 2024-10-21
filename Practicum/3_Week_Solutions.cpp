e52619b91cdae5ed7aaf4759dea76a153cbd4acc
tyhristova <teodora.yu.hristova@gmail.com>
2024-10-21 17:05:27 +0300

    Update 3_Week_Solutions.cpp

--- Practicum/3_Week_Solutions.cpp
+++ Practicum/3_Week_Solutions.cpp
@@ -1,1 +1,55 @@
+// Week 3.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+	//int n = 0, m = 0;
+
+	//std::cin >> n >> m;
+
+	//if (n <= 256 && n >= 0 && m <= 256 && m >= 0)
+	//{
+	//	
+	//	for (int i = n; i < m; i++)
+	//	{
+	//		char currentLetter = i;
+	//		std::cout << i << " " << "---->" << currentLetter << std::endl;
+
+	//	}
+	//}
+	//else
+	//{
+	//	std::cout << "Invalid codes!";
+	//}
+
+	//int num = 0, i = 1;
+	//
+	//std::cin >> num;
+	//int factorial = 1;
+
+	//while (i <= num)
+	//{
+	//	
+	//	 factorial *= i++;
+	//}
+
+	//std::cout << factorial;
+
+	//int num = 0, sum = 0;
+
+	//std::cin >> num;
+
+	//for (int i = 1; i <= num; i++)
+	//{
+	//	for (int j = 1; j <= i; j++)
+	//	{
+	//		sum += j;
+	//	} //----> sum+= i*(i+1)/2;
+	//}
+
+	//std::cout << sum;
+
+}
 
