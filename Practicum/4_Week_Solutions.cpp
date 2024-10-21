7dbd321021fddb4240cbb5e4652caff1b7284a68
GeorgiParnarev <131038567+GeorgiParnarev@users.noreply.github.com>
2024-10-21 16:59:38 +0300

    Update 4_Week_Solutions.cpp
    M

--- Practicum/4_Week_Solutions.cpp
+++ Practicum/4_Week_Solutions.cpp
@@ -1,1 +1,57 @@
 
+#include <iostream>
+
+int IsPrime(int n)
+{
+	if (n == 1)
+	{
+		return false;
+	}
+
+	if (n % 2 == 0 || n % 3 == 0)
+	{
+		return true;
+	}
+
+	int length = sqrt(n);
+
+	for (int i = 5; i <= length; i += 6)
+	{
+		if (n % i == 0 || n % (i + 2) == 0)
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
+int main()
+{
+	// zad 1
+
+	int n;
+	std::cin >> n;
+
+	int size = sqrt(n);
+	int* array = new int[size];
+	int arrayLength = 0;
+
+	for (int i = 1; i < 20; i++)
+	{
+		if (IsPrime(i))
+		{
+			array[arrayLength] = i;
+
+			arrayLength++;
+		}
+	}
+
+	for (int i = 0; i < arrayLength; i++)
+	{
+		for (int j = i; j < arrayLength; j++)
+		{
+
+		}
+	}
+}
