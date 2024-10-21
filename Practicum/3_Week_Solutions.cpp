2faecdf0517877cd5e6ea0df0f496c2d77ff6919
bjnnb <martinn.radulov@gmail.com>
2024-10-21 17:00:06 +0300

    Update 3_Week_Solutions.cpp

--- Practicum/3_Week_Solutions.cpp
+++ Practicum/3_Week_Solutions.cpp
@@ -1,1 +1,63 @@
+//zad 1
+/*int n, m;
+int asciiNumber = 0;
+std::cin >> n >> m;
+if (!(n >= 0 && m <= 256)) {
 
+	std::cout << "Enter valid data";
+
+}
+else {
+	for (int i = n; i < m; i++) {
+
+			std::cout << i << " -->" << char(i) << std::endl;
+
+		}
+
+}*/
+
+//zad 2
+/*int n = 1;
+int factorial = n;
+std:: cin >> n;
+for (int i = 0; i < n; i++) {
+
+	factorial *= n - i;
+
+
+}
+std::cout << factorial;*/
+
+//zad 3
+//unsigned int N = 1, sum = 0;
+//std::cout << "Enter a natural number"<<std::endl;
+//std::cin >> N;
+////zashto tozi for spira da raboti za chisla po golemi ot 5
+///*for (int i = 1; i <= N; i++) {
+
+//	sum += i;
+//	sum += i + 1;
+
+//}*/
+//for (int i = 1; i <= N; i++) {
+//	for (int j = 1; j < i; j++) {
+
+//		sum += j;
+
+//	}
+//	sum += i;
+
+//}
+//std::cout << sum;
+
+//zad 4
+/*int n = 1;
+std::cin >> n;
+for (int i = 1; i < n; i++) {
+	if (n % i == 0) {
+
+		std::cout << i << std::endl;
+
+	}
+
+}*/
