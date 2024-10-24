bf798c0f452f82408ab8a6f20dd8e90df62844bc
Stanislav Trendafilov <114089296+Stanislav-Trendafilov@users.noreply.github.com>
2024-10-24 16:36:37 +0300

    Update 4_Week_Solutions.cpp

--- Practicum/4_Week_Solutions.cpp
+++ Practicum/4_Week_Solutions.cpp
@@ -87,15 +87,176 @@ int main()
 	}*/
 
 	//5
-	int n;
+	/*int n, count = 1;
 	std::cin >> n;
-	for (int i = 0; i < n; i++)
+	for (int i = 1; i <= n; i++)
 	{
-		for (int j = 0; j < n; j++)
+		count = 1;
+		for (int j = 1; j <= i; j++)
 		{
-
+			std::cout << count++;
+			if (i == n && j == i)
+			{
+				for (int k = 0; k < n; k++)
+				{
+					std::cout << '-';
+				}
+			}
 		}
+		for (int p = i + 1; p <= 3 * n; p++)
+		{
+			if (i != n)
+			{
+				std::cout << " ";
+			}
+			if (p == 3 * n - i)
+			{
+				for (int k = 3 * n - i; k < 3 * n; k++)
+				{
+					std::cout << (count - 1);
+					count--;
+				}
+			}
+		}
+		std::cout << std::endl;
 	}
+	for (int i = n-1; i > 0; i--)
+	{
+		count = 1;
+		for (int j = 1; j <= i; j++)
+		{
+			std::cout << count++;
+			if (i == n && j == i)
+			{
+				for (int k = 0; k < n; k++)
+				{
+					std::cout << '-';
+				}
+			}
+		}
+		for (int p = i + 1; p <= 3 * n; p++)
+		{
+			if (i != n)
+			{
+				std::cout << " ";
+			}
+			if (p == 3 * n - i)
+			{
+				for (int k = 3 * n - i; k < 3 * n; k++)
+				{
+					std::cout << (count - 1);
+					count--;
+				}
+			}
+		}
+
+
+		std::cout << std::endl;
+	}*/
+
+	//6
+	int n, count = 1;
+	std::cin >> n;
+	for (int i = 1; i <= n + 2; i++)
+	{
+		std::cout << "#";
+	}
+	std::cout << std::endl;
+	bool lastElReached = false;
+	for (int i = 1; i <= n; i++)
+	{
+		count = 0;
+		lastElReached = false;
+		std::cout << "#";
+		for (int j = i; j < n + i; j++)
+		{
+			if (j > n)
+			{
+				lastElReached = true;
+				for (int k = 1; k <= n; k++)
+				{
+					if (count == n)
+					{
+						break;
+					}
+					std::cout << k;
+					count++;
+
+				}
+			}
+			if (lastElReached)
+			{
+				break;
+			}
+			std::cout << j;
+			count++;
+		}
+		std::cout << "#" << std::endl;
+	}
+	for (int i = 1; i < n + 2; i++)
+	{
+		if (i == 1 || i == n + 1)
+		{
+			std::cout <<"#";
+		}
+
+		if (n % 2 == 0)
+		{
+			if (i == (n + 2) / 2|| i == (n + 2) / 2-1)
+			{
+				std::cout << "X";
+				continue;
+			}
+		}
+		else
+		{
+			if (i == (n + 2) / 2)
+			{
+				std::cout << "X";
+				continue;
+			}
+		}
+		std::cout << " ";
+		
+		
+	}
+	std::cout << std::endl;
+	for (int i = n; i >0; i--)
+	{
+		count = 0;
+		lastElReached = false;
+		std::cout << "#";
+		for (int j = i; j < n + i; j++)
+		{
+			if (j > n)
+			{
+				lastElReached = true;
+				for (int k = 1; k <= n; k++)
+				{
+					if (count == n)
+					{
+						break;
+					}
+					std::cout << k;
+					count++;
+
+				}
+			}
+			if (lastElReached)
+			{
+				break;
+			}
+			std::cout << j;
+			count++;
+		}
+		std::cout << "#" << std::endl;
+	}
+	for (int i = 1; i <= n + 2; i++)
+	{
+		std::cout << "#";
+	}
+
+	//
 
 
 }
