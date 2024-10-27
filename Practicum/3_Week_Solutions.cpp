0caa48a4516c749fa16d5c4c48343c70d5992601
SvetoslaV1312 <84904098+SvetoslaV1312@users.noreply.github.com>
2024-10-27 11:22:45 +0200

    3_Week_Solutions.cpp Svetoslav Tsvyatkov

--- Practicum/3_Week_Solutions.cpp
+++ Practicum/3_Week_Solutions.cpp
@@ -1,1 +1,103 @@
 
+#include <iostream>
+
+int main()
+{
+	/*
+	* task 1
+	int start, end;
+	std::cin >> start >> end;
+	if (n>=0 && m<=256)
+	{
+	std::cerr<<"Invalid codes!"; return 1;
+	}
+	char s=start, e=end;
+	for (; s <= e; s++)
+	{
+		std::cout << s << std::endl;
+	}
+
+	task 2
+	int factorial=1, n;
+	std::cin >> n;
+	for (int i = 2; i <= n; i++)
+	{
+		factorial*=i;
+	}
+	std::cout << factorial << std::endl;
+
+	task 3
+	int n, i, sum=0, l_sum=0;
+	std::cin >> n;
+	for (i = 1; i <= n; i++)
+	{
+		sum += i+l_sum;
+		l_sum += i;
+	}
+	std::cout << sum;
+
+	task 4
+	int n;
+	std::cin >> n;
+	for(int i=1; i<=n; i++)
+	{
+		if (n % i==0) std::cout << i << std::endl;
+	}
+
+	task 5
+
+	int n ,x=0, y=1, print;
+	std::cin >> n;
+		if (n < 2)
+	{
+		std::cout << "Wrong input.";
+			return 1;
+	}
+	else if (n==2) {
+	cout<<x<<" "<<y;
+	return 0;
+	}
+	for (int i = 2; i < n; i++)
+	{
+
+		print = x + y;
+		std::cout << print<<" ";
+		x = y;
+		y = print;
+	}
+
+	task 6
+
+	int n, p=0, sum=0;
+	std::cin >> n;
+	while (p != -1)
+	{
+		std::cin >> p;
+		if (p < 15 && p % 2 == 0) sum += p;
+	}
+	std::cout << sum<<std::endl;
+
+
+
+	int m, n;
+	std::cin >> n >> m;
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= m; j++)
+			std::cout << "( " << i<<" ,"<< j<<") ";
+		std::cout << std::endl;
+	}
+
+
+	int n, m, sum=0;
+	std::cin >> n >> m;
+	for (; n <= m; n++)
+	{
+		for (int i = 1; i < n; i++)
+		{
+			if (n % i == 0)sum += i;
+		}
+		if (n == sum)std::cout << sum<<std::endl;
+		sum = 0;
+	}
+*/
