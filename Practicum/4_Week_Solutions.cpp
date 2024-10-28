11eb864bd57018ed72e676b46095a265284fb536
tyhristova <teodora.yu.hristova@gmail.com>
2024-10-28 17:02:52 +0200

    Update 4_Week_Solutions.cpp

--- Practicum/4_Week_Solutions.cpp
+++ Practicum/4_Week_Solutions.cpp
@@ -1,1 +1,129 @@
+#include <iostream>
+
+int absoluteValue(int number)
+{
+	return (number > 0) ? number : -number;
+}
+
+bool isDigit(char symbol)
+{
+
+}
+
+char toUpper(char symbol)
+{
+	return symbol + 'A' - 'a';
+}
+
+char toLower(char symbol)
+{
+	return symbol + 'a' - 'A'
+}
+
+#include <iostream>
+
+int absoluteValue(int number)
+{
+	return (number > 0) ? number : -number;
+}
+
+bool isDigit(char symbol)
+{
+
+}
+
+char toUpper(char symbol)
+{
+	return symbol + 'A' - 'a';
+}
+
+char toLower(char symbol)
+{
+	return symbol + 'a' - 'A'
+}
+
+int toNumber(char symbol)
+{
+	if (symbol <= '9' && symbol >= '0')
+	{
+		return int(symbol - '0');
+	}
+}
+
+
+
+int main()
+{
+	// 1
+	//int num = 0;
+	//bool isPrime = true;
+
+	//std::cin >> num;
+
+	//for (int i = 2; i * i < num; i++)    // or  i <= num/2
+	//{
+	//	if (num % i == 0)
+	//	{
+	//		isPrime = false;
+	//		break;
+	//	}
+	//}
+
+	//std::cout << isPrime << std::endl;
+
+	// 2
+	//unsigned long number = 0;
+	//int digit = 0;
+	//int counter = 0;
+
+	//std::cin >> number;
+
+	//while (number > 0)
+	//{
+	//	int rndm = 
+	//}
+	
+
+
+}
+
+
+
+
+
+int main()
+{
+	// 1
+	//int num = 0;
+	//bool isPrime = true;
+
+	//std::cin >> num;
+
+	//for (int i = 2; i * i < num; i++)    // or  i <= num/2
+	//{
+	//	if (num % i == 0)
+	//	{
+	//		isPrime = false;
+	//		break;
+	//	}
+	//}
+
+	//std::cout << isPrime << std::endl;
+
+	// 2
+	//unsigned long number = 0;
+	//int digit = 0;
+	//int counter = 0;
+
+	//std::cin >> number;
+
+	//while (number > 0)
+	//{
+	//	int rndm = 
+	//}
+	
+
+
+}
+
 
