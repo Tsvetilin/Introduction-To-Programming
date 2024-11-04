36ab3d0f2c8933c3084f3ad2da3a690b1b30b95a
tyhristova <teodora.yu.hristova@gmail.com>
2024-11-04 17:04:54 +0200

    Teodora Practicum_Solutions.cpp

--- Week 05/Practicum_Solutions.cpp
+++ Week 05/Practicum_Solutions.cpp
@@ -1,1 +1,59 @@
+// week5.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+// 1
+void swap(int& a, int& b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void swap(double& a, double& b) {
+    double temp = a;
+    a = b;
+    b = temp;
+}
+
+// 7
+int gcd(int first, int second){
+
+    if (!(first > second)) {
+        swap(first, second);
+    }
+  
+    for (int i = second; i >= 1; --i) {
+        if (first % i == 0 && second % i == 0) {
+             return i;
+         }      
+    }
+    
+
+}
+
+//2
+void f(int& nominator, int& denominator) {
+    int gcdInput = gcd(nominator, denominator);
+    int newNominator = nominator / gcdInput;
+    int newDenominator = denominator / gcdInput;
+
+    
+}
+
+
+
+
+int main()
+{
+    int nominator;
+    int denominator;
+
+    std::cin >> nominator >> denominator;
+
+    f(nominator, denominator);
+
+    std::cout << 
+
+}
 
