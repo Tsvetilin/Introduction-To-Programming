7cb1089519c8d6dfc041764994ac8f7a94e567b0
Felipe Urra <87881375+felipeUrra@users.noreply.github.com>
2024-11-17 14:34:05 +0200

    Update preparation_exam1.cpp

--- Practicum/preparation_exam1.cpp
+++ Practicum/preparation_exam1.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 
-int gettingSmallerSum(int arr[][100], int, int, int);
-int gettingSum(int arr[][100], int, int);
+void gettingSmallestSum(int arr[][100], int, int, int);
+int gettingSum(int arr[][100], int, int, int, int, int);
 
 int main()
 {
     int originalArr[100][100];
 
-    uint mLen, nLen;
+    uint row, col;
     std::cout << "Input the lenght of the array: ";
-    std::cin >> mLen >> nLen;
+    std::cin >> row >> col;
 
     std::cout << "Input the elements of the array";
-    for (size_t i = 0; i < mLen; i++)
+    for (size_t i = 0; i < row; i++)
     {
-        for (size_t j = 0; j < nLen; j++)
+        for (size_t j = 0; j < col; j++)
         {
             std::cin >> originalArr[i][j];
         }
@@ -24,31 +24,44 @@ int main()
     uint subArrLen;
     std::cin >> subArrLen;
 
+    gettingSmallestSum(originalArr, row, col, subArrLen);
+
 }
 
-int gettingSmallerSum(int original[][100], int mLen, int nLen, int subArrLen)
+void gettingSmallestSum(int original[][100], int row, int col, int subArrLen)
 {
     int sum = 0;
     int auxSum = 0;
+    int index[] = {0, 0};
 
 
-    for (size_t i = 0; i < nLen - (subArrLen - 1); i++)
+    for (size_t i = 0; i < row - (subArrLen - 1); i++)
     {
-        for (size_t j = 0; j < mLen - (subArrLen - 1); j++)
+        for (size_t j = 0; j < col - (subArrLen - 1); j++)
         {
-            auxSum = gettingSum(original, j, i);
+            auxSum = gettingSum(original, i, j, row, col, subArrLen);
             
-            if (i == 0) sum = auxSum;
+            if (i == 0 && j == 0) sum = auxSum;
 
-            if (sum < auxSum) sum = auxSum;
+            if (sum > auxSum) sum = auxSum, index[0] = i, index[1] = j;
+        }
+    }
+    
+    std::cout << sum << " (" << index[0] << "," << index[1] << ")";
+}
+
+int gettingSum(int original[][100], int i, int j, int row, int col, int subArrLen)
+{
+    int sum = 0;
+
+    for (size_t x = i; x < subArrLen + i; x++)
+    {
+        for (size_t y = j; y < subArrLen + j; y++)
+        {
+            sum += original[x][y];
         }
     }
     
     return sum;
 }
-
-int gettingSum(int original[][100], int j, int i)
-{
-    int sum = original[j][i] + original[j][i + 1] + original[j + 1][i] + original[j + 1][i + 1];
-    return sum;
 }
