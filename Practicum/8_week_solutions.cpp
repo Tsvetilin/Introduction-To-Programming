60e00e9d1e6f59f3e9cb80206bcd861ff232befa
Felipe Urra <87881375+felipeUrra@users.noreply.github.com>
2024-12-03 01:49:26 +0200

    Update 8_week_solutions.cpp
    1 and 2 solutions added

--- Practicum/8_week_solutions.cpp
+++ Practicum/8_week_solutions.cpp
@@ -18,14 +18,29 @@ int makeBitZero(unsigned int n, unsigned int ind);
 // 1
 int cutBinary(unsigned int n, unsigned int ind1, unsigned int ind2)
 {
-    int mask = flipBit(n, ind2);
+    unsigned int amountOfNumsFromInd1 = ind1 + 1;
+    unsigned int amountOfNumsFromInd2 = amountOfNumsFromInd1 - ind2;
 
-    return (n ^ mask);
+    return n >> (amountOfNumsFromInd2);
+}
+
+// 2
+int getAmountOfOnesInBinary(unsigned int n)
+{
+    uint counter = 0;
+
+    for (size_t i = 0; i <= 31; i++) if (checkBit(n, i)) counter++;
+    
+    return counter;
 }
 
 int main()
 {
-    
+    // 1
+    printBinary(cutBinary(16, 4, 2));
+
+    // 2
+    std::cout << getAmountOfOnesInBinary(189) << '\n';
 }
 
 bool checkBit(unsigned int n, unsigned int ind)
@@ -37,7 +52,7 @@ bool checkBit(unsigned int n, unsigned int ind)
 
 void printBinary(unsigned int n)
 {
-    for (short i = 31; i > 0; i--)
+    for (short i = 31; i >= 0; i--)
     {
         std::cout << checkBit(n, i);
     }
