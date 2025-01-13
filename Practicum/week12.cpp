4852468f25fe36caaa9b10886daf17af94397536
Felipe Urra <87881375+felipeUrra@users.noreply.github.com>
2025-01-13 16:38:02 +0200

    Update week12.cpp
    1 and 4 added

--- Practicum/week12.cpp
+++ Practicum/week12.cpp
@@ -6,15 +6,41 @@ bool amountCapitalLetters(char* str, int i);
 bool amountDigits(char* str, int i);
 bool amountWhitespaces(char* str, int i);
 
-// Task with recursivity
+// Tasks with recursivity
 // 1
+bool fibo(int*, int, int, int);
+
+// 4
+bool amountDeterminatedChar(char * str, char c, int & amount, int index);
 
 int main()
 {
-    char str[] = " I am a Businessman 123 ";
-    std::cout << countCharsApplicableTo(str, amountCapitalLetters) << '\n';
-    std::cout << countCharsApplicableTo(str, amountDigits) << '\n';
-    std::cout << countCharsApplicableTo(str, amountWhitespaces) << '\n';
+    // Task with higher order functions
+    //char str[] = " I am a Businessman 123 ";
+    //std::cout << countCharsApplicableTo(str, amountCapitalLetters) << '\n';
+    //std::cout << countCharsApplicableTo(str, amountDigits) << '\n';
+    //std::cout << countCharsApplicableTo(str, amountWhitespaces) << '\n';
+
+    // Tasks with recursivity
+    // 1
+    /*const int n = 6;
+    int arr[n + 1] = { 1, 1 };
+    fibo(arr, n, 0, 1);
+
+    for (size_t i = 0; i < n; i++)
+    {
+        std::cout << arr[i] << ' ';
+    }*/
+
+    // 4
+    char str[] = "alabala";
+    char c = 'a';
+    int amount = 0;
+    const int initialIndex = 0;
+
+    amountDeterminatedChar(str, c, amount, initialIndex);
+    std::cout << amount << '\n';
+
 }
 
 
@@ -38,7 +64,7 @@ bool amountCapitalLetters(char* str, int i)
     {
         return true;
     }
-    
+
     return false;
 }
 
@@ -57,6 +83,31 @@ bool amountWhitespaces(char* str, int i)
     if (*(str + i) == ' ')
     {
         return true;
+    }
+
+    return false;
+}
+
+bool fibo(int* arr, int n, int first, int second)
+{
+    while (second != n)
+    {
+        *(arr + second + 1) = *(arr + first) + *(arr + second);
+        first++;
+        second++;
+        fibo(arr, n, first, second);
+    }
+
+    return false;
+}
+
+bool amountDeterminatedChar(char * str, char c, int & amount, int index)
+{
+    while (*(str + index) != '\0')
+    {
+        if (*(str + index) == c) amount++;
+        amountDeterminatedChar(str, c, amount, ++index);
+        return false;
     }
 
     return false;
